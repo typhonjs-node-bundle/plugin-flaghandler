7d06c00f478490926ca3c14a65b9d68dff64b612
typhonrt <support@typhonrt.org>
2021-03-16 15:18:06 -0700

    initial ErrorHandler refactor

--- src/errors/ErrorHandler.js
+++ src/errors/ErrorHandler.js
@@ -61,6 +61,62 @@ export class ErrorHandler
    }
 
    /**
+    * Attempts to load the first package.json found in the stack trace as the source of the offending error and returns
+    * a formatted object of the loaded `package.json`.
+    *
+    * @param {Array} trace - The stack trace formatted from `typhonjs-color-logger`.
+    *
+    * @returns {object|undefined}   The formatted `package.json` if found from stack trace.
+    */
+   _formatFromTrace(trace)
+   {
+      let packageInfo = null;
+      let foundMatch = null;
+
+      if (Array.isArray(trace))
+      {
+         let modulePath = null;
+
+         // Walk through the stack trace array of strings until the first entry that matches the five regex tests below.
+         // This defines the module path to attempt to load any associated `package.json` file.
+         for (const entry of trace)
+         {
+            if (modulePath !== null) { break; }
+
+            for (const match of this._match)
+            {
+               const matches = match.exec(entry);
+               modulePath = matches !== null && matches.length >= 1 ? matches[1] : void 0;
+               if (typeof modulePath === 'string') { foundMatch = match; break; }
+            }
+
+            for (const match of s_DEFAULT_MATCH)
+            {
+               const matches = match.exec(entry);
+               modulePath = matches !== null && matches.length >= 1 ? matches[1] : void 0;
+               if (typeof modulePath === 'string') { foundMatch = match; break; }
+            }
+         }
+
+         if (typeof modulePath === 'string')
+         {
+            try
+            {
+               const packageObj = JSON.parse(fs.readFileSync(`${modulePath}package.json`, 'utf8'));
+
+               packageInfo = PackageUtil.format(packageObj);
+            }
+            catch (packageErr) { /* nop */ }
+         }
+      }
+
+      return {
+         packageInfo,
+         match: foundMatch
+      };
+   }
+
+   /**
     * @param {Error} error - Error to handle / log.
     *
     * @param {Errors} Errors - @oclif/core Errors instance.
@@ -90,7 +146,7 @@ export class ErrorHandler
       // package.json for the detected NPM module and post a fatal log message noting as much.
       if (traceInfo)
       {
-         matchData = s_FORMAT_FROM_TRACE(traceInfo.trace);
+         matchData = this._formatFromTrace(traceInfo.trace);
       }
 
       // Note: This will exclude any package that starts w/ @oclif from posting a detailed error message. Since this is
@@ -98,11 +154,45 @@ export class ErrorHandler
       // detected where a `package.json` file can be found.
       if (matchData !== null)
       {
-         s_PRINT_ERR_MESSAGE(matchData, error);
+         this._printErrMessage(matchData, error);
       }
 
       return Errors.handle(error);
    }
+
+   /**
+    * Prints an error message with `typhonjs-color-logger` including the package data info and error.
+    *
+    * @param {object}   matchData - Data object potentially containing packageInfo and match data.
+    * @param {object}   matchData.packageInfo - Formatted `package.json` data.
+    * @param {object}   matchData.match - The associated match
+    *
+    * @param {Error}    error - An uncaught error.
+    */
+   _printErrMessage(matchData, error)
+   {
+      let message = '';
+
+      // Create a specific message if the module matches.
+      if (matchData.packageInfo !== null)
+      {
+         const sep =
+            '-----------------------------------------------------------------------------------------------\n';
+
+         message = matchData.match !== null ? `${matchData.match.message}\n${sep}` : `s_DEFAULT_MESSAGE\n${sep}`;
+
+         message += `${matchData.packageInfo.formattedMessage}\n${global.$$cli_name_version}`;
+
+         // Log any uncaught errors as fatal.
+         logger.fatal(message, sep, error, '\n');
+      }
+      else
+      {
+         // Log any uncaught errors as fatal.
+         logger.fatal(`An unknown fatal error has occurred; ${global.$$cli_name_version}:`, error, '\n');
+      }
+   }
+
 
    /**
     * Wires up ErrorHandler on the plugin eventbus.
@@ -120,92 +210,3 @@ export class ErrorHandler
 }
 
 export default new ErrorHandler();
-
-/**
- * Attempts to load the first package.json found in the stack trace as the source of the offending error and returns
- * a formatted object of the loaded `package.json`.
- *
- * @param {Array} trace - The stack trace formatted from `typhonjs-color-logger`.
- *
- * @returns {object|undefined}   The formatted `package.json` if found from stack trace.
- */
-function s_FORMAT_FROM_TRACE(trace)
-{
-   let packageInfo = null;
-   let foundMatch = null;
-
-   if (Array.isArray(trace))
-   {
-      let modulePath = null;
-
-      // Walk through the stack trace array of strings until the first entry that matches the five regex tests below.
-      // This defines the module path to attempt to load any associated `package.json` file.
-      for (const entry of trace)
-      {
-         if (modulePath !== null) { break; }
-
-         for (const match of this._match)
-         {
-            const matches = match.exec(entry);
-            modulePath = matches !== null && matches.length >= 1 ? matches[1] : void 0;
-            if (typeof modulePath === 'string') { foundMatch = match; break; }
-         }
-
-         for (const match of s_DEFAULT_MATCH)
-         {
-            const matches = match.exec(entry);
-            modulePath = matches !== null && matches.length >= 1 ? matches[1] : void 0;
-            if (typeof modulePath === 'string') { foundMatch = match; break; }
-         }
-      }
-
-      if (typeof modulePath === 'string')
-      {
-         try
-         {
-            const packageObj = JSON.parse(fs.readFileSync(`${modulePath}package.json`, 'utf8'));
-
-            packageInfo = PackageUtil.format(packageObj);
-         }
-         catch (packageErr) { /* nop */ }
-      }
-   }
-
-   return {
-      packageInfo,
-      match: foundMatch
-   };
-}
-
-/**
- * Prints an error message with `typhonjs-color-logger` including the package data info and error.
- *
- * @param {object}   matchData - Data object potentially containing packageInfo and match data.
- * @param {object}   matchData.packageInfo - Formatted `package.json` data.
- * @param {object}   matchData.match - The associated match
- *
- * @param {Error}    error - An uncaught error.
- */
-function s_PRINT_ERR_MESSAGE(matchData, error)
-{
-   let message = '';
-
-   // Create a specific message if the module matches.
-   if (matchData.packageInfo !== null)
-   {
-      const sep =
-         '-----------------------------------------------------------------------------------------------\n';
-
-      message = matchData.match !== null ? `${matchData.match.message}\n${sep}` : `s_DEFAULT_MESSAGE\n${sep}`;
-
-      message += `${matchData.packageInfo.formattedMessage}\n${global.$$cli_name_version}`;
-
-      // Log any uncaught errors as fatal.
-      logger.fatal(message, sep, error, '\n');
-   }
-   else
-   {
-      // Log any uncaught errors as fatal.
-      logger.fatal(`An unknown fatal error has occurred; ${global.$$cli_name_version}:`, error, '\n');
-   }
-}
