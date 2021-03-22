7d0d860a4c74342f79ab057f6112c5a2d640baf6
typhonrt <support@typhonrt.org>
2021-03-22 09:52:53 -0700

    initial update for Oclif error handler parity.

--- src/errors/ErrorHandler.js
+++ src/errors/ErrorHandler.js
@@ -38,83 +38,129 @@ export class ErrorHandler
     */
    handle(error, Errors)
    {
-      // Given a magic boolean variable assigned to an error skip printing out a fatal error.
-      if (error instanceof NonFatalError || (typeof error.$$error_fatal === 'boolean' && !error.$$error_fatal))
+      try
       {
-         const logEvent = typeof error.$$logEvent === 'string' ? error.$$logEvent : 'log:error';
-         const errorCode = Number.isInteger(error.$$errorCode) ? error.$$errorCode : 1;
+         if (!error) { error = new Error('no error?'); }
+         if (error.message === 'SIGINT') { process.exit(1); }
 
-         // log error message unless the log event is `log:trace`.
-         global.$$eventbus.trigger(logEvent, logEvent !== 'log:trace' ? error.message : error);
+         // Given a magic boolean variable assigned to an error skip printing out a fatal error.
+         if (error instanceof NonFatalError || (typeof error.$$error_fatal === 'boolean' && !error.$$error_fatal))
+         {
+            const logEvent = typeof error.$$logEvent === 'string' ? error.$$logEvent : 'log:error';
+            const errorCode = Number.isInteger(error.$$errorCode) ? error.$$errorCode : 1;
 
-         return Errors.handle(new Errors.ExitError(errorCode));
+            // log error message unless the log event is `log:trace`.
+            global.$$eventbus.trigger(logEvent, logEvent !== 'log:trace' ? error.message : error);
+
+            return Errors.handle(new Errors.ExitError(errorCode));
+         }
+
+         // Acquire trace info from '@typhonjs-node-utils/error-parser'
+         const normalizedError = errorParser.normalize({ error });
+         const filterError = errorParser.filter({ error });
+
+         const normalizedPackageObj = PackageUtil.getPackageAndFormat({
+            filepath: normalizedError.firstFilepath,
+            callback: (data) => typeof data.packageObj.name === 'string'
+         });
+
+         const filterPackageObj = PackageUtil.getPackageAndFormat({
+            filepath: filterError.firstFilepath,
+            callback: (data) => typeof data.packageObj.name === 'string'
+         });
+
+         let bitfield = 0;
+         bitfield |= normalizedPackageObj !== void 0 ? 1 : 0;
+         bitfield |= filterPackageObj !== void 0 ? 2 : 0;
+
+         // Handle the cases when both filtered and normalized error stacks are the same or the filtered stack is empty.
+         if (normalizedError.uuid === filterError.uuid || filterError.stack.length === 0)
+         {
+            bitfield = 1;
+         }
+
+         let message = `An uncaught fatal error has occurred.`;
+
+         switch (bitfield)
+         {
+            case 0:
+               message += ` ${global.$$cli_name_version}:\n${normalizedError.toString()}`;
+               break;
+            case 1:
+               message += s_MESSAGE_ONE_MODULE;
+
+               message += `\n\n${s_MESSAGE_SEPARATOR}\n${normalizedPackageObj.formattedMessage}\n`;
+               message += `CLI: ${global.$$cli_name_version}\nError UUID: ${normalizedError.uuid}\n\n`;
+               message += `${normalizedError.toString()}${s_MESSAGE_SEPARATOR}`;
+               break;
+
+            case 2:
+               message += s_MESSAGE_ONE_MODULE;
+
+               message += `\n\n${s_MESSAGE_SEPARATOR}\n${filterPackageObj.formattedMessage}\n`;
+               message += `CLI: ${global.$$cli_name_version}\nError UUID: ${filterError.uuid}\n\n`;
+               message += `${filterError.toString()}${s_MESSAGE_SEPARATOR}`;
+               break;
+
+            case 3:
+               message += s_MESSAGE_TWO_MODULE;
+
+               message += `\n\n${s_MESSAGE_SEPARATOR}\n${filterPackageObj.formattedMessage}\n`;
+               message += `CLI: ${global.$$cli_name_version}\nError UUID: ${filterError.uuid}\n\n`;
+               message += `${filterError.toString()}${s_MESSAGE_SEPARATOR}`;
+
+               message += `\n\n${s_MESSAGE_SEPARATOR}\n${normalizedPackageObj.formattedMessage}\n`;
+               message += `CLI: ${global.$$cli_name_version}\nError UUID: ${normalizedError.uuid}\n\n`;
+               message += `${normalizedError.toString()}${s_MESSAGE_SEPARATOR}`;
+               break;
+         }
+
+         // Log any uncaught errors as fatal.
+         logger.fatal(message, '\n');
+
+         return Errors.handle(error);
       }
-
-      // Acquire trace info from '@typhonjs-node-utils/error-parser'
-      const normalizedError = errorParser.normalize({ error });
-      const filterError = errorParser.filter({ error });
-
-      const normalizedPackageObj = PackageUtil.getPackageAndFormat({
-         filepath: normalizedError.firstFilepath,
-         callback: (data) => typeof data.packageObj.name === 'string'
-      });
-
-      const filterPackageObj = PackageUtil.getPackageAndFormat({
-         filepath: filterError.firstFilepath,
-         callback: (data) => typeof data.packageObj.name === 'string'
-      });
-
-      let bitfield = 0;
-      bitfield |= normalizedPackageObj !== void 0 ? 1 : 0;
-      bitfield |= filterPackageObj !== void 0 ? 2 : 0;
-
-      // Handle the cases when both filtered and normalized error stacks are the same or the filtered stack is empty.
-      if (normalizedError.uuid === filterError.uuid || filterError.stack.length === 0)
+      catch (err)
       {
-         bitfield = 1;
+         console.error(error.stack);
+         console.error(err.stack);
+         process.exit(1);
       }
-
-      let message = `An uncaught fatal error has occurred.`;
-
-      switch (bitfield)
-      {
-         case 0:
-            message += ` ${global.$$cli_name_version}:\n${normalizedError.toString()}`;
-            break;
-         case 1:
-            message += s_MESSAGE_ONE_MODULE;
-
-            message += `\n\n${s_MESSAGE_SEPARATOR}\n${normalizedPackageObj.formattedMessage}\n`;
-            message += `CLI: ${global.$$cli_name_version}\nError UUID: ${normalizedError.uuid}\n\n`;
-            message += `${normalizedError.toString()}${s_MESSAGE_SEPARATOR}`;
-            break;
-
-         case 2:
-            message += s_MESSAGE_ONE_MODULE;
-
-            message += `\n\n${s_MESSAGE_SEPARATOR}\n${filterPackageObj.formattedMessage}\n`;
-            message += `CLI: ${global.$$cli_name_version}\nError UUID: ${filterError.uuid}\n\n`;
-            message += `${filterError.toString()}${s_MESSAGE_SEPARATOR}`;
-            break;
-
-         case 3:
-            message += s_MESSAGE_TWO_MODULE;
-
-            message += `\n\n${s_MESSAGE_SEPARATOR}\n${filterPackageObj.formattedMessage}\n`;
-            message += `CLI: ${global.$$cli_name_version}\nError UUID: ${filterError.uuid}\n\n`;
-            message += `${filterError.toString()}${s_MESSAGE_SEPARATOR}`;
-
-            message += `\n\n${s_MESSAGE_SEPARATOR}\n${normalizedPackageObj.formattedMessage}\n`;
-            message += `CLI: ${global.$$cli_name_version}\nError UUID: ${normalizedError.uuid}\n\n`;
-            message += `${normalizedError.toString()}${s_MESSAGE_SEPARATOR}`;
-            break;
-      }
-
-      // Log any uncaught errors as fatal.
-      logger.fatal(message, '\n');
-
-      return Errors.handle(error);
    }
 }
+
+// TODO evaluate removing the use of Errors.handle and incorporate the key aspects of the Oclif error handler directly.
+// TODO Reason: Errors print twice when using Error.handle. Below is the Oclif error handler.
+/*
+  try {
+    if (!err) err = new CLIError('no error?')
+    if (err.message === 'SIGINT') process.exit(1)
+
+    const shouldPrint = !(err instanceof ExitError)
+    const pretty = prettyPrint(err)
+    const stack = clean(err.stack || '', {pretty: true})
+
+    if (shouldPrint) {
+      console.error(pretty ? pretty : stack)
+    }
+
+    const exitCode = err.oclif?.exit !== undefined && err.oclif?.exit !== false ? err.oclif?.exit : 1
+
+    if (config.errorLogger && err.code !== 'EEXIT') {
+      if (stack) {
+        config.errorLogger.log(stack)
+      }
+
+      config.errorLogger.flush()
+      .then(() => process.exit(exitCode))
+      .catch(console.error)
+    } else process.exit(exitCode)
+  } catch (error) {
+    console.error(err.stack)
+    console.error(error.stack)
+    process.exit(1)
+  }
+
+ */
 
 export default new ErrorHandler();
